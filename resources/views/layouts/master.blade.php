b770d1d7031d2e70380dedf8b96e95d930a6bd95
cod3mau5 <code.bit.mau@gmail.com>
2022-01-23 17:39:23 -0700

    new tawk

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -253,18 +253,13 @@
                 (function(){
                 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
                 s1.async=true;
-                s1.src='https://embed.tawk.to/5fbc4dcba1d54c18d8eca8cd/default';
+                s1.src='https://embed.tawk.to/61edf2af9bd1f31184d8e36a/1fq4nnvjl';
                 s1.charset='UTF-8';
                 s1.setAttribute('crossorigin','*');
                 s0.parentNode.insertBefore(s1,s0);
                 })();
-
-                window.Tawk_API = window.Tawk_API || {};
-                window.Tawk_API.customStyle = {
-                    display : 'none'
-                };
               </script>
-              
               <!--End of Tawk.to Script-->
+
           </body>
       </html>
